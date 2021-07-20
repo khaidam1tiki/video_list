08ba5833ed69c17bff22f38dcd2f7859a333d682
Khai Dam <quangkhaidam93@gmail.com>
2021-07-20 15:30:26 +0700

    Fixed bug layout for Sale Products in small screen

--- src/pages/Home/components/SaleProductItem/index.js
+++ src/pages/Home/components/SaleProductItem/index.js
@@ -1,3 +1,5 @@
 Component({
-
+  props: {
+    itemWidth: undefined
+  }
 });
--- src/pages/Home/components/SaleProducts/index.js
+++ src/pages/Home/components/SaleProducts/index.js
@@ -1,3 +1,12 @@
-Component({
+import myx from "../../../../services/myx";
 
+Component({
+  data: {
+    itemWidth: undefined,
+  },
+  async onInit() {
+    const systemInfo = await myx.getSystemInfo();
+    console.log("khai 123 screenWidth", systemInfo.screenWidth);
+    this.setData({itemWidth: systemInfo.screenWidth * (85 / 100)});
+  }
 });
